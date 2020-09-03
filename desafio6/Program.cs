55a55146299d8509d2dec80ebaa0edac462cc4aa
matiasojeda13 <matias.ojeda@correo.ucu.edu.com>
2020-09-02 23:07:20 -0300

    creada la carpeta del desafio 6

---
+++ desafio6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace desafio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
